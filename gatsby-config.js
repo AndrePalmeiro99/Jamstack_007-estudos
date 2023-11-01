167edac33c512f6ab2081a187994ba7d7d9460d0
André Palmeiro <andrepalmeiro14@gmail.com>
2023-11-01 11:28:49 -0300

    commit inicial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,6 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Estudos de Jamstack com Gatsby`,
   },
   plugins: [],
 }
